7b1bd8969f7e8f5ee514744266a1fb962b86671c
Luca <lucasommer@gmx.ch>
2020-12-10 21:56:25 +0100

    more php stuff

--- server.php
+++ server.php
@@ -1,5 +1,6 @@
 <?php
 
+
     switch ($_POST['type']) {
         case "login":
             login($_POST['username'], $_POST['password']);
@@ -19,22 +20,47 @@
 
     function register($username, $password){
         $conn = openDbCon();
+
         $stmt = $conn->prepare("INSERT INTO players (Username, Password) VALUES (?, ?)");
+
+        $password = md5($password);
 
         $stmt->bind_param("ss", $username, $password);
 
         $stmt->execute();
 
-        createCookie($username);
-        print_r("success");
+        createCookie(md5($username));
+
+        getPlayers();
+
+        echo("success");
 
         $stmt->close();
     }
 
-    function createCookie($value) {
-        $cookie_name = "blaster_usr";
-        $cookie_value = $value;
-        setcookie($cookie_name, $cookie_value, time() + (86400 * 7), "/"); // 86400 = 1 day
+    function getPlayers(){
+        $conn = openDbCon();
+
+        $sql = "SELECT ID, Username, Password, Highscore FROM players";
+        $result = $conn->query($sql);
+        
+        if ($result->num_rows > 0) {
+          while($row = $result->fetch_assoc()) {
+            
+
+            echo "id: " . $row["ID"]. " - Name: " . $row["Username"]. " " . $row["Password"]. "<br>";
+          }
+        } else {
+          echo "0 results";
+        }
+    }
+
+    function createCookie($cookieValue) {
+        setcookie("blaster", $cookieValue, time() + (86400 * 7), "/"); // 86400 = 1 day
+    }
+
+    function getCookie() {
+        return $_COOKIE["blaster"];
     }
 
     function openDbCon() {
@@ -45,18 +71,10 @@
         $conn = new mysqli($dbhost, $dbuser, $dbpass, $db);
 
         if ($conn->connect_error) {
-            print_r("Connection failed: " . $conn->connect_error);
+            die("Connection failed: " . $conn->connect_error);
         } else {
             return $conn;
         }
     }
 
-    /*
-    $conn = OpenCon();
-    echo "YES";
-    CloseCon($conn);
-    */
-
-
-    
 ?>
